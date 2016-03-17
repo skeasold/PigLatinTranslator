1f50c29e2d7a93bb703f18426f58dad3f4eef57a
Skylar King <sking@crazyawesome.io>
2016-03-16 22:11:46 -0500

    Add vowel rules.

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,5 +1,7 @@
 class PigLatin
   def self.translate(word)
-    "#{word}ay"
+    first_letter = word[0].downcase.split("")
+    vowels = ["a", "e", "i", "o", "u"]
+    (first_letter & vowels).empty? ? (return "#{word}") : (return "#{word}ay")
   end
 end
