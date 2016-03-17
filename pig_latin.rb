c8f79806f5ee056164a224a64e584d1f5e503fa1
Skylar King <sking@crazyawesome.io>
2016-03-16 22:44:50 -0500

    Added consonants.

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,7 +1,17 @@
 class PigLatin
   def self.translate(word)
     first_letter = word[0].downcase.split("")
-    vowels = ["a", "e", "i", "o", "u"]
-    (first_letter & vowels).empty? ? (return "#{word}") : (return "#{word}ay")
+
+    vowels = %w(a e i o u)
+    consonants = %w(b c d f g h j k l m n p q r s t v x z)
+
+    if (first_letter & vowels).empty? == false
+      return "#{word}ay"
+    elsif (first_letter & consonants).empty? == false
+      return "#{word[1..-1]}#{word[0]}ay"
+    else
+      return "#{word}"
+    end
+
   end
 end
